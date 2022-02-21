67da0e4119804acbfa84c1bc9f46f28a828cb934
mbreithecker <m@breithecker.de>
2022-02-21 10:03:40 -0700

    added ProposalEnden and Voted Event

--- src/types/events.ts
+++ src/types/events.ts
@@ -15,22 +15,39 @@ export class MessageEvent {
 
   constructor(eventsArray: any[], decodedTx: FullDecodedTransaction) {
     this.eventsArray = eventsArray;
-    this.action = eventsArray.find((value) => value.key == "action").value;
-    this.module = eventsArray.find((value) => value.key == "module").value;
-    this.sender = eventsArray.find((value) => value.key == "sender").value;
+    this.action = eventsArray.reverse().find((value) => value.key == "action").value;
+    this.module = eventsArray.reverse().find((value) => value.key == "module").value;
+    this.sender = eventsArray.reverse().find((value) => value.key == "sender").value;
     this.time = decodedTx.blockTime;
     this.height = decodedTx.indexedTx?.height;
 
     this.args = {};
     if (
-      this.action == "fund_pool" ||
-      this.action == "defund_pool" ||
-      this.args == "stake_pool" ||
-      this.args == "unstake_pool"
+      this.action == "Funded" ||
+      this.action == "Defunded" ||
+      this.args == "Staked" ||
+      this.args == "Unstaked"
     ) {
       this.args.creator = this.get("Creator");
       this.args.poolId = this.get("Id");
       this.args.amount = this.get("Amount");
+    } else
+    if(this.action == "ProposalEnded") {
+      this.args.bundleId = this.get("BundleId");
+      this.args.byteSize = this.get("ByteSize");
+      this.args.uploader = this.get("Uploader");
+      this.args.nextUploader = this.get("NextUploader");
+      this.args.bundleReward = this.get("BundleReward");
+      this.args.valid = this.get("Valid");
+      this.args.fromHeight = this.get("FromHeight");
+      this.args.toHeight = this.get("ToHeight");
+      this.args.status = this.get("Status");
+    } else
+    if (this.action == "Voted") {
+      this.args.creator = this.get("Creator");
+      this.args.poolId = this.get("Id");
+      this.args.bundleId = this.get("BundleId");
+      this.args.support = this.get("Support");
     }
   }
 
