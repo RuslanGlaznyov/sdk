4d9f715323520d937acbfd006c2740c59fbc5d6e
John Letey <john@kyve.network>
2022-03-24 11:17:50 +0100

    maybe ...

--- src/utils/registry.ts
+++ src/utils/registry.ts
@@ -1,34 +1,17 @@
 import { Registry } from "@cosmjs/proto-signing";
 import { cosmos } from "@keplr-wallet/cosmos";
 import path from "path";
-import { Field, load, Type } from "protobufjs";
-
-const Content = new Type("content")
-  .add(new Field("@type", 1, "string"))
-  .add(new Field("title", 2, "string"))
-  .add(new Field("description", 3, "string"))
-  .add(new Field("id", 4, "uint64"))
-  .add(new Field("name", 5, "string"))
-  .add(new Field("runtime", 6, "string"))
-  .add(new Field("logo", 7, "string"))
-  .add(new Field("versions", 8, "string"))
-  .add(new Field("config", 9, "string"))
-  .add(new Field("start_height", 10, "uint64"))
-  .add(new Field("min_bundle_size", 11, "uint64"))
-  .add(new Field("operating_cost", 12, "uint64"));
-
-const MsgSubmitProposal = new Type("MsgSubmitProposal")
-  .add(Content)
-  // @ts-ignore
-  .add(new Field("initial_deposit", 2, cosmos.base.v1beta1.Coin, "repeated"))
-  .add(new Field("proposer", 3, "string"));
+import { load } from "protobufjs";
 
 export const createRegistry = async (): Promise<Registry> => {
   const root = await load(path.join(__dirname, "../proto/tx.proto"));
 
   return new Registry(
     Array.from([
-      [`/cosmos.gov.v1beta1.MsgSubmitProposal`, MsgSubmitProposal],
+      [
+        `/cosmos.gov.v1beta1.MsgSubmitProposal`,
+        root.lookupType("MsgSubmitProposal"),
+      ],
       [`/cosmos.gov.v1beta1.MsgDeposit`, cosmos.gov.v1beta1.MsgDeposit],
       [`/cosmos.gov.v1beta1.MsgVote`, cosmos.gov.v1beta1.MsgVote],
       [
