4e8ca37aa89f636fdc0eda3af2076c72fdd3e01b
ruslan glaznyov <ruslan.glaznyov@gmail.com>
2022-06-21 18:25:53 +0300

    yarn build

--- dist/client/kyve.web.client.d.ts
+++
@@ -1,8 +0,0 @@
-import KyveClient from "./kyve.client";
-import { Client } from "../types/client";
-import { AccountData } from "@cosmjs/amino/build/signer";
-export default class KyveWebClient extends KyveClient {
-    private readonly walletName;
-    constructor(client: Client, account: AccountData, walletName: string);
-    getWalletName(): string;
-}
--- test/test1.js
+++
@@ -1,9 +0,0 @@
-import KyveSDK from "../dist/index" ;
-const MNEMONIC =
-	"vacant vivid bench divorce expire math fix method foam industry clinic machine";
-(async function main() {
-	const sdk = new KyveSDK('korellia');
-	const kyveClient = await sdk.fromMnemonic(MNEMONIC)
-	const r = await kyveClient.nativeClient.getChainId()
-	console.log(r);
-})()
