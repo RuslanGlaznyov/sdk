545375c51ec6e130cbeb3e433a1516628786460f
ruslan glaznyov <ruslan.glaznyov@gmail.com>
2022-06-21 13:11:46 +0300

    added dist

---
+++ dist/client/kyve-base.msg.d.ts
@@ -0,0 +1,24 @@
+import { MsgClaimUploaderRole, MsgDefundPool, MsgDelegatePool, MsgFundPool, MsgStakePool, MsgSubmitBundleProposal, MsgUndelegatePool, MsgUnstakePool, MsgUpdateMetadata, MsgVoteProposal, MsgWithdrawPool } from "@kyve/proto/dist/proto/kyve/registry/v1beta1/tx";
+import { StdFee } from "@cosmjs/amino/build/signdoc";
+import { AccountData } from "@cosmjs/amino/build/signer";
+import { MessageEvent } from "../types/events";
+import { Client } from "../types/client";
+export default class KyveBaseMsg {
+    private nativeClient;
+    readonly account: AccountData;
+    constructor(client: Client, account: AccountData);
+    foundPool(value: MsgFundPool, fee?: StdFee | "auto" | number, memo?: string): Promise<import("@cosmjs/stargate").DeliverTxResponse>;
+    defundPool(value: MsgDefundPool, fee?: StdFee | "auto" | number, memo?: string): Promise<import("@cosmjs/stargate").DeliverTxResponse>;
+    stakePool(value: MsgStakePool, fee?: StdFee | "auto" | number, memo?: string): Promise<import("@cosmjs/stargate").DeliverTxResponse>;
+    unstakePool(value: MsgUnstakePool, fee?: StdFee | "auto" | number, memo?: string): Promise<import("@cosmjs/stargate").DeliverTxResponse>;
+    delegatePool(value: MsgDelegatePool, fee?: StdFee | "auto" | number, memo?: string): Promise<import("@cosmjs/stargate").DeliverTxResponse>;
+    withdrawPool(value: MsgWithdrawPool, fee?: StdFee | "auto" | number, memo?: string): Promise<import("@cosmjs/stargate").DeliverTxResponse>;
+    undelegatePool(value: MsgUndelegatePool, fee?: StdFee | "auto" | number, memo?: string): Promise<import("@cosmjs/stargate").DeliverTxResponse>;
+    submitBundleProposal(value: MsgSubmitBundleProposal, fee?: StdFee | "auto" | number, memo?: string): Promise<import("@cosmjs/stargate").DeliverTxResponse>;
+    voteProposal(value: Omit<MsgVoteProposal, "creator">, fee?: StdFee | "auto" | number, memo?: string): Promise<import("@cosmjs/stargate").DeliverTxResponse>;
+    claimUploaderRole(value: MsgClaimUploaderRole, fee?: StdFee | "auto" | number, memo?: string): Promise<import("@cosmjs/stargate").DeliverTxResponse>;
+    updateMetadata(value: MsgUpdateMetadata, fee?: StdFee | "auto" | number, memo?: string): Promise<import("@cosmjs/stargate").DeliverTxResponse>;
+    transfer(recipient: string, amount: string, fee?: StdFee | "auto" | number, memo?: string): Promise<import("@cosmjs/stargate").DeliverTxResponse>;
+    getMessageEventLogs(fromBlock: number, toBlock: number): Promise<MessageEvent[]>;
+    getKyveBalance(): Promise<string>;
+}
